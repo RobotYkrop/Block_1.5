86d91e85880f0c5a71f17952055f38448d4e2be4
RobotYkrop <dendozzor@gmail.com>
2022-03-26 18:26:11 +0500

    commit

--- script-slider.js
+++ script-slider.js
@@ -4,45 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const desktop = window.matchMedia('(min-width: 1025px)');
 
 function swiperMode() {
-    
-  const swiper = new Swiper(".mySwiper", {
-    pagination: {
-    el: ".swiper-pagination",
-  },
-    breakpoints: {
-    
-320: {
-slidesPerView: 2,
-spaceBetween: 13
-},
 
-480: {
-slidesPerView: 3,
-spaceBetween: 23
-},
-
-640: {
-slidesPerView: 4,
-spaceBetween: 33
-},
-},
-
-})
 
 if(mobile.matches) {
-  swiper.init()
-        }
-// Disable (for tablet)
-else if(tablet.matches) {
-    swiper.destroy()
-    
-}
+    const swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        breakpoints: {
 
-// Disable (for desktop)
-else if(desktop.matches) {
-    swiper.destroy()
-    
-}
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 13
+            },
+
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 23
+            },
+
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 33
+            },
+        },
+
+    })
+        }
 }
 
 /* On Load
